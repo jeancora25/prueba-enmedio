a98e8f99ed65a0550a3a5278235e0699f83b264e
jeancora25 <jeancora25@gmail.com>
2024-06-12 18:10:43 -0500

    code clean

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
 
 export default function Layout(props) {
   const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
